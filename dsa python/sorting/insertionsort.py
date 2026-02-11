096b4db0d68f09aff740dede202f325a3b013510
Eshani-s-nair <eshanisnair@gmail.com>
2026-02-11 21:58:17 +0530

    add insertion sorting

---
+++ dsa python/sorting/insertionsort.py
@@ -0,0 +1,11 @@
+def insertion_sort(arr):
+    n=len(arr)
+    for i in range(1,n):
+        insert_index=i
+        current_value=arr.pop(i)
+        for j in range(i-1,-1,-1):
+            if arr[j]>current_value:
+                insert_index=j
+        arr.insert(insert_index,current_value)
+    print(arr)
+insertion_sort([7,12,9,11,3])
